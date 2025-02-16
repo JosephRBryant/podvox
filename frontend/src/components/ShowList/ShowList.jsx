e41101517d299df86d6c7f2e8cb75dbbc990cbd6
Joseph Bryant <josephroybryant@gmail.com>
2025-02-15 20:53:41 -0600

    created map for show tiles

--- frontend/src/components/ShowList/ShowList.jsx
+++ frontend/src/components/ShowList/ShowList.jsx
@@ -1,9 +1,19 @@
+import { useSelector } from "react-redux"
 import { getAllShowsThunk } from "../../redux/show"
 import './ShowList.css'
 
 const ShowList = () => {
+  let shows = useSelector(state => state.showState.allShows);
+
   return (
-    null
+    <>
+      {shows.map((show, idx) => (
+        <div key={`${idx}-${show.title}`} className="show-card-container">
+          <h2>Show Title</h2>
+          <p>{show.showTitle}</p>
+        </div>
+      ))}
+    </>
   )
 
 }
