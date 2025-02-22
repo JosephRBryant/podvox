0e4dc3fcfe1b0f844f4ab68b5483d34154850532
Joseph Bryant <josephroybryant@gmail.com>
2025-02-22 11:18:50 -0600

    Created navbar for sort and genre

--- frontend/src/components/ShowList/ShowList.jsx
+++ frontend/src/components/ShowList/ShowList.jsx
@@ -6,13 +6,26 @@ const ShowList = () => {
   let shows = useSelector(state => state.showState.allShows);
 
   return (
-    <>
+    <main>
+      <h1>Podcasts</h1>
+      <div className="shows-navbar">
+        <form action="">
+          <label htmlFor="sort"></label>
+          <select name="sort" id="sort" placeholder="sort">
+            <option value="" disabled selected hidden>Sort by...</option>
+            <option value="name">Podcast Name</option>
+            <option value="popular">Trending</option>
+            <option value="recent">Recent Episodes</option>
+            <option value="show-count">Episode Count</option>
+          </select>
+        </form>
+      </div>
       {shows.map((show, idx) => (
         <div key={`${idx}-${show.title}`} className="show-card-container">
           <ShowCard show={show}/>
         </div>
       ))}
-    </>
+    </main>
   )
 
 }
--- frontend/src/router/index.jsx
+++ frontend/src/router/index.jsx
@@ -26,7 +26,7 @@ export const router = createBrowserRouter([
         element: <SignupFormPage />,
       },
       {
-        path: "/shows",
+        path: "shows",
         element: <ShowList />
       },
       {
