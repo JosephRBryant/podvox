115bfc40e32f66deaff047445321e6329dc21015
Joseph Bryant <josephroybryant@gmail.com>
2025-03-29 22:26:06 -0500

    Clamping show card description

--- frontend/src/components/ShowCard/ShowCard.jsx
+++ frontend/src/components/ShowCard/ShowCard.jsx
@@ -1,6 +1,7 @@
 import './ShowCard.css'
 import getAvgLength from '../../helpers/get-avg-length';
 import formatDate from '../../helpers/format-date';
+import { IoPlayOutline } from "react-icons/io5";
 
 const ShowCard = ({show}) => {
   const episodes = show.Episodes;
@@ -28,33 +29,58 @@ const ShowCard = ({show}) => {
 
       <img className="show-card-img" src={show.showImage} alt="" />
       <div className="show-card-content">
-        <p>{show.showTitle}</p>
-        <p>{show.showDesc}</p>
-        <div className="show-card-stats">
-          <div className="show-card-ep-count">
-            {!show.Episodes || show.Episodes.length === 0 ? (
-              `No published episodes`
-            ) : (
-              pubEpisodes.length === 1 ? (
-                `${pubEpisodes.length} episode`
+        <div className="show-card-copy">
+          <h3 className='show-card-title'>{show.showTitle}</h3>
+          {show.showDesc.length > 433 ? (
+            <>
+            <p className='show-card-desc'>
+              {`${show.showDesc} `}
+            </p>
+            {show.showDesc.length > 300 && (
+              <a className="show-card-more-link" href={`/shows/${show.id}`}>...more</a>
+            )
+            }
+          </>
 
+          ) : (
+            <p className='show-card-desc'>{show.showDesc}</p>
+
+          )}
+
+        </div>
+        <div className="show-card-stats-listen">
+          <div className="show-card-stats">
+            <div className="show-card-ep-count">
+              {!show.Episodes || show.Episodes.length === 0 ? (
+                `No published episodes`
               ) : (
-                `${pubEpisodes.length} episodes`
-              )
+                pubEpisodes.length === 1 ? (
+                  `${pubEpisodes.length} episode`
+
+                ) : (
+                  `${pubEpisodes.length} episodes`
+                )
+              )}
+            </div>
+            {!hasEpisodes ? (
+              null
+            ) : (
+              <>
+                |
+                <div className="show-card-avg-length">
+                  {`Avg Length: ${getAvgLength(pubEpisodes)}`}
+                </div>
+                |
+                <div className="show-card-newest-date">
+                  {`Last: ${formatDate(getNewestEpisodeDate())}`}
+                </div>
+              </>
             )}
           </div>
-          {!hasEpisodes ? (
-            null
-          ) : (
-            <>
-              <div className="show-card-avg-length">
-                {`Average Length ${getAvgLength(pubEpisodes)}`}
-              </div>
-              <div className="show-card-newest-date">
-                {`Latest episode ${formatDate(getNewestEpisodeDate())}`}
-              </div>
-            </>
-          )}
+          <button className="show-card-listen-btn">
+          <IoPlayOutline className='show-card-play-icon'/>
+          Listen Now
+          </button>
         </div>
       </div>
     </>
