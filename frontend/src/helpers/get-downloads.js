e7b350fdbe51e38e7626e56dff35989c0bec9f8b
Joseph Bryant <josephroybryant@gmail.com>
2025-04-26 23:38:00 -0500

    readressed downloads function

--- frontend/src/helpers/get-downloads.js
+++ frontend/src/helpers/get-downloads.js
@@ -2,7 +2,7 @@ export const getDownloads = (arr) => {
   return arr.sort((a,b) => b.showDownloadTotal - a.showDownloadTotal)
 }
 
-export const sortEpisodes = (arr) => {
+export const downloads = (arr) => {
   return arr.sort((a,b) => b.downloads - a.downloads)
 }
 
